b2e6da52d4b98b70d5249e1907607182cdf8ba7f
vpatel025 <140524949+vpatel025@users.noreply.github.com>
2023-08-22 15:44:14 -0400

    adding extrusion and slitting

--- home.js
+++ home.js
@@ -1,8 +1,11 @@
 const machines = [
-  { value: 'Pouch2', apiUrl: 'http://10.0.0.70/api/v0/process_state/reason' },
-  { value: 'Pouch5', apiUrl: 'http://10.0.0.106/api/v0/process_state/reason' },
-  { value: 'Pouch6', apiUrl: 'http://10.0.0.117/api/v0/process_state/reason' },
-  { value: 'Genesis', apiUrl: 'http://10.0.0.144/api/v0/process_state/reason' },
+  { value: 'Pouch2', apiUrl: 'http://10.0.0.69/api/v0/process_state/reason' },
+  { value: 'Pouch5', apiUrl: 'http://10.0.0.51/api/v0/process_state/reason' },
+  { value: 'Pouch6', apiUrl: 'http://10.0.0.64/api/v0/process_state/reason' },
+  { value: 'Genesis', apiUrl: 'http://10.0.0.60/api/v0/process_state/reason' },
+  { value: 'Deacro1', apiUrl: 'http://10.0.0.109/api/v0/process_state/reason' },
+  { value: 'Deacro2', apiUrl: 'http://10.0.0.68/api/v0/process_state/reason' },
+  { value: 'Cason', apiUrl: 'http://10.0.0.39/api/v0/process_state/reason' },
 ];
 
 document.addEventListener('DOMContentLoaded', function () {
